d0c84ef8482cadf88b01b1a29514d45b18b055c9
VictoriaArengas <134026117+VictoriaArengas@users.noreply.github.com>
2023-07-28 00:19:01 -0500

    Add files via upload
    Avance de consultas 2

--- Consultas_ejercicio3SQL.sql
+++ Consultas_ejercicio3SQL.sql
@@ -5,16 +5,13 @@ SELECT film_id, title FROM film;
 SELECT * FROM film LIMIT 5;
 
 /*3. Selecciona filas de la tabla film donde film_id sea menor que 4.*/
-SELECT * FROM film WHERE film_id <=4; 
+SELECT * FROM film WHERE film_id <4;
 
 /*4. Selecciona filas de la tabla film donde el rating sea PG o G.*/
 SELECT * FROM film WHERE rating ='PG' OR rating ='G'; 
 
 /*5. Selecciona filas de la tabla actor donde el nombre sea Angela, Angelina o Audrey usando IN.*/
-SELECT * FROM actor WHERE first_name ='Angela' OR first_name ='Angelina' OR first_name ='Audrey';
-SELECT * FROM actor WHERE first_name IN(34,65,76,144,190);
-
-
+SELECT * FROM actor WHERE first_name IN('Angela','Angelina','Audrey');
 
 /*6. Obtén una lista de actores con el nombre Julia.*/
 SELECT * FROM actor WHERE first_name ='Julia';
@@ -26,30 +23,37 @@ SELECT * FROM actor WHERE first_name ='Chris' OR first_name = 'Cameron' OR first
 SELECT * FROM customer WHERE first_name ='Jamie' AND last_name ='Rice';
 
 /*9.Selecciona el monto y la fecha de pago de la tabla payment donde el monto pagado sea menor a $1.*/
-SELECT amount, payment_date FROM payment WHERE amount <=1;
+SELECT amount, payment_date FROM payment WHERE amount <1;
 
 /*10.¿Cuáles son las diferentes duraciones de alquiler permitidas por la tienda?*/
-SELECT return_date FROM rental ORDER BY return_date;
-
+SELECT * AVG (return_date) FROM rental GROUP BY return_date;
+SELECT * FROM rental;
 
 
 /*11.Ordena las filas en la tabla city por country_id y luego por city.*/
+SELECT * FROM city WHERE country_id ='Julia' ORDER BY country_id, city ;
+SELECT * FROM city ORDER BY country_id;
 
 
 /*12.¿Cuáles son los ID de los últimos 3 clientes que devolvieron un alquiler?*/
+SELECT customer_id FROM rental ORDER BY last_update;
 
 
 /*13.¿Cuántas películas tienen clasificación NC-17? ¿Cuántas tienen clasificación PG o PG-13?*/
-
+SELECT * FROM film;
+SELECT COUNT (rating) FROM film WHERE rating = 'NC-17'; 
+SELECT COUNT (rating) FROM film WHERE rating ='PG' OR rating ='PG-13';
 
 /*14.¿Cuántos clientes diferentes tienen registros en la tabla rental?*/
-
+SELECT COUNT (DISTINCT customer_id) FROM rental;
 
 /*15.¿Hay algún cliente con el mismo apellido?*/
+SELECT last_name COUNT (*) FROM customer GROUP BY last_name ORDER last_name;
 
 
 /*16.¿Qué película (id) tiene la mayor cantidad de actores?*/
-
+SELECT * FROM film;
+SELECT MAX (film_id) FROM film WHERE rating = 'NC-17'; 
 
 /*17.¿Qué actor (id) aparece en la mayor cantidad de películas?*/
 
